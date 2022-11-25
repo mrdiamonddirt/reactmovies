81707dec1db6235bdbeb15fc04b12cbf2eb73f02
RowDog <mrdiamonddirt@gmail.com>
2022-11-25 10:36:22 +0000

    fixed app clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useEffect, useState} from 'react';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=93a52616'
+
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+const [movies, setMovies] = useState([]);
+
+const searchMovies = async (title) => {
+  const response = await fetch(`${API_URL}&s=${title}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+useEffect(() => {
+  searchMovies("Batman");
+}, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
