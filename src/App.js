964adcd30688cb69656ef92f6e0f39240b7ad52f
RowDog <mrdiamonddirt@gmail.com>
2022-11-30 14:32:26 +0000

    fixes login and token

--- src/App.js
+++ src/App.js
@@ -1,87 +1,86 @@
-import React from 'react';
-import './App.css';
-import {useEffect, useState} from 'react';
+import React from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 // import MovieCard from './components/MovieCard';
-import Login from './components/Login';
-import Register from './components/Register';
-import DeleteUser from './components/DeleteUser';
-import UpdateUser from './components/UpdateUser';
-import AllUsers from './components/AllUsers';
-import { writeCookie } from './common';
-import { getCookie } from './common';
-import { findUser } from './utils';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import DeleteUser from "./components/DeleteUser";
+import UpdateUser from "./components/UpdateUser";
+import AllUsers from "./components/AllUsers";
+import { writeCookie } from "./common";
+import { getCookie } from "./common";
+import { findUser } from "./utils";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-
 function App() {
-
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
   const [register, setRegister] = useState(false);
 
   const [user, setUser] = useState();
   const [users, setUsers] = useState();
 
-  console.log('users is:', users);
-const searchMovies = async (title) => {
-  const response = await fetch(`${API_URL}&s=${title}`);
-  const data = await response.json();
-  setMovies(data.Search);
-  // console.log(data);
-}
+  console.log("users is:", users);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    // console.log(data);
+  };
 
-const handleRegisterBtn = () => {
-  if (register) {
-    setRegister(false);
-  } else {
-    setRegister(true);
-  }
-}
+  const handleRegisterBtn = () => {
+    if (register) {
+      setRegister(false);
+    } else {
+      setRegister(true);
+    }
+  };
 
-useEffect(() => {
-  let cookie = getCookie('jwt_woken');
-  if (cookie !== false) {
-    loginWithToken(cookie);
-  }
-  // searchMovies("Batman");
-}, []);
+  useEffect(() => {
+    let cookie = getCookie("jwt_token");
+    if (cookie !== false) {
+      loginWithToken(cookie);
+    }
+    // searchMovies("Batman");
+  }, []);
 
-const loginWithToken = async (cookie) => {
- const user = await findUser(cookie);
-  setUser(user);
-}
-
-
+  const loginWithToken = async (cookie) => {
+    const user = await findUser(cookie);
+    setUser(user);
+  };
 
   return (
     <div className="App">
-      <Login setter={setUser}/>
-      {user ?
-      <> 
-      <h1>Welcome {user}</h1> 
-      <UpdateUser user={user}/>
-      <DeleteUser user={user}/>
-      </>
-      : 
-      <h1>Please Login</h1>}
-      <button onClick={() => {handleRegisterBtn()}}>Register</button>
-      {register ? <Register setter={setUser}/>
-       : 
-       null
-       }
+      <Login setter={setUser} />
+      {user ? (
+        <>
+          <h1>Welcome {user}</h1>
+          <UpdateUser user={user} />
+          <DeleteUser user={user} />
+        </>
+      ) : (
+        <h1>Please Login</h1>
+      )}
+      <button
+        onClick={() => {
+          handleRegisterBtn();
+        }}
+      >
+        Register
+      </button>
+      {register ? <Register setter={setUser} /> : null}
       <AllUsers setter={setUsers} />
 
+      {/* conditional renders when logged in */}
+      {/* if specific user return all users from database and display in table */}
+      {/* if specific user allow creation of records and deletion of records  */}
 
-        {/* conditional renders when logged in */}
-        {/* if specific user return all users from database and display in table */}
-        {/* if specific user allow creation of records and deletion of records  */}
+      {/* create change password conditional render with 2 inputs to compare the 2 passwords  */}
 
-        {/* create change password conditional render with 2 inputs to compare the 2 passwords  */}
+      {/* create delete own user button that deletes does another password check and deletes user */}
 
-        {/* create delete own user button that deletes does another password check and deletes user */}
-
-        {/* create a button to add a movie to the database */}
+      {/* create a button to add a movie to the database */}
 
       {/* <h1>My Movies App</h1>
 
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,132 +1,124 @@
 import { writeCookie } from "../common";
 
 export const loginUser = async (name, email, password, setter) => {
-    try {
-        const response = await fetch("http://localhost:5001/loginUser", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                },
-            body: JSON.stringify({
-                "name": name,
-                "email": email,
-                "password": password
-            }),
-    
-        });
+  try {
+    const response = await fetch("http://localhost:5001/loginUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+    });
 
-        const data = await response.json();
-        console.log(data)
-        setter(data.name)
-        writeCookie("jwt_token", data.token, 7)
+    const data = await response.json();
+    console.log(data);
+    setter(data.user);
+    writeCookie("jwt_token", data.token, 7);
+  } catch (error) {
+    console.log(error);
+  }
 
-    } catch (error) {
-        console.log(error)
-        
-    }
+  // create crud functions for users
 
-    // create crud functions for users
-    
-    // add a delete own user function
+  // add a delete own user function
+};
 
-    }
+export const findUser = async (cookie) => {
+  try {
+    const response = await fetch("http://localhost:5001/loginUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookie}`,
+      },
+    });
 
-    export const findUser = async (cookie) => {
-        try {
-            const response = await fetch("http://localhost:5001/findUser", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${cookie}`
-                    },
-            });
-    
-            const data = await response.json();
-            console.log(data)
-            return (data.username)
-        } catch (error) {
-            console.log(error)
-            
-        }
-    }
+    const data = await response.json();
+    console.log(data);
+    return data.name;
+  } catch (error) {
+    console.log(error);
+  }
+};
 // add a new user to the database function
 export const registerUser = async (name, email, password, setter) => {
-    try {
-        const response = await fetch("http://localhost:5001/addUser", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                },
-            body: JSON.stringify({
-                "name": name,
-                "email": email,
-                "password": password
-            }),
-        })
-        const data = await response.json();
-        console.log(data)
-        setter(data.name)
-    } catch (error) {
-        console.log(error)
-    }    
-}
+  try {
+    const response = await fetch("http://localhost:5001/addUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    setter(data.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // get all users from the database
 export const getAllUsers = async (setter) => {
-    try {
-        const response = await fetch("http://localhost:5001/getUsers", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            }
-        })
-        const data = await response.json();
-        console.log(data)
-        setter(data.data.users)
-
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const response = await fetch("http://localhost:5001/getUsers", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    setter(data.data.users);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const Updateuser = async (user, key, value) => {
-    try {
-        const response = await fetch("http://localhost:5001/updateUser", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    "user": user,
-                    "key": key,
-                    "value": value
-                }),
-        })
-        const data = await response.json();
-        console.log(data)
-        return (data)
-    } catch (error) {
-        console.log(error)
-        
-    }
+  try {
+    const response = await fetch("http://localhost:5001/updateUser", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: user,
+        key: key,
+        value: value,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const deleteUser = async (user) => {
-    try {
-        const response = await fetch("http://localhost:5001/deleteUser", {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    "user": user,
-                }),
-        })
-        const data = await response.json();
-        console.log(data)
-        return (data)
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+  try {
+    const response = await fetch("http://localhost:5001/deleteUser", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: user,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
